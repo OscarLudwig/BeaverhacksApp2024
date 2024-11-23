03b2dab61ec773c63a904eda8321394452809ec2
Llarence <1010kai.t1010@gmail.com>
2024-11-22 23:50:15 -0800

    update variable feed

--- beaver-hacks/src/app/campuslife/campuspage.js
+++ beaver-hacks/src/app/campuslife/campuspage.js
@@ -1,11 +1,12 @@
 import CampusEvent from "./campusevent";
 import { getRssFeed } from "./rss"
 
-export default async function FoodPage() {
-  const feed = await getRssFeed("https://news.oregonstate.edu/releases/feed/campus-life");
+export default async function CampusPage({ url, title }) {
+  const feed = await getRssFeed(url);
 
   return (
     <div>
+      <h1>{title}</h1>
       {feed.map((value, index) => (
         <CampusEvent key={index} title={value.title} link={value.link} name={value.creator} />
       ))}
--- beaver-hacks/src/app/page.js
+++ beaver-hacks/src/app/page.js
@@ -3,6 +3,9 @@ import CampusPage from "./campuslife/campuspage";
 
 export default function Home() {
   return (
-    <CampusPage />
+    <div>
+      <CampusPage title="Athletics" url="https://news.oregonstate.edu/releases/feed/athletics" />
+      <CampusPage title="General" url="https://news.oregonstate.edu/releases/feed/" />
+    </div>
   );
 }

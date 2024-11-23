cc8ba28fa64643393a9e29186b9889292c92b47a
Llarence <1010kai.t1010@gmail.com>
2024-11-23 10:17:14 -0800

    add date

--- beaver-hacks/src/app/events/campusevent.js
+++ beaver-hacks/src/app/events/campusevent.js
@@ -4,6 +4,8 @@ export default function CampusEvent(props) {
       <a className="campustitle" href={props.link}>{props.title}</a>
       <br></br>
       <span>by {props.name}</span>
+      <br></br>
+      <span>{new Date(Date.parse(props.date)).toDateString()}</span>
     </div>
   );
 }

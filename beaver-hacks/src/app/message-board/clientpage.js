30e6ab9bea50246f7fd6c88db096630d0a6fea80
Llarence <1010kai.t1010@gmail.com>
2024-11-24 13:01:51 -0800

    add updating without refresh

--- beaver-hacks/src/app/message-board/clientpage.js
+++ beaver-hacks/src/app/message-board/clientpage.js
@@ -1,19 +1,9 @@
 "use client"
 
-import { useMemo } from "react";
 import PostBox from "./postbox";
 import Message from "./message";
 
-export default function ClientPage({ posts, page }) {
-  // This should maybe be a prop
-  const username = useMemo(() => {
-    try {
-      return JSON.parse(atob(token.split('.')[1])).username;
-    } catch (error) {
-      return undefined;
-    }
-  })
-
+export default function ClientPage({ posts, page, username }) {
   function switchPage(newPage) {
     window.location.href = `/message-board?page=${newPage}`;
   }
@@ -22,7 +12,7 @@ export default function ClientPage({ posts, page }) {
     <div>
       {posts.map((value, index) => (
         <Message key={index} id={value._id} title={value.title} author={value.author} body={value.body}
-          votes={value.votes} upVote={value.upVote} downVote={value.downVote} loggedin={username !== undefined} />
+          inVotes={value.votes} inUpVote={value.upVote} inDownVote={value.downVote} loggedin={username !== undefined} />
       ))}
       <PostBox enabled={username} />
       <button onClick={() => switchPage(page <= 1 ? 0 : page - 1)}>Previous</button>
--- beaver-hacks/src/app/message-board/page.js
+++ beaver-hacks/src/app/message-board/page.js
@@ -15,8 +15,10 @@ export default async function MessageBoard({ searchParams }) {
   }
 
   let token = (await cookies()).get('auth_token');
+  let username = undefined;
   if (token) {
     token = token.value;
+    username = JSON.parse(atob(token.split('.')[1])).username
   }
 
   const response = await fetch(process.env.URL + "/api/forum", {
@@ -31,7 +33,7 @@ export default async function MessageBoard({ searchParams }) {
       <TitleBar />
       <main className="main">
         <h1>Message Board</h1>
-        <ClientPage posts={posts} page={page} />
+        <ClientPage posts={posts} page={page} username={username} />
       </main>
     </div>
   );

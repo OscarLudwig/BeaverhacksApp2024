52b4c6f108732e33a6a6c5cb65bb4456614fc428
Oscar Ludwig <134669025+OscarLudwig@users.noreply.github.com>
2024-11-23 19:50:23 -0800

    changed page to displaying in rows of 3

--- beaver-hacks/src/app/food/clientfoodpage.js
+++ beaver-hacks/src/app/food/clientfoodpage.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import FoodPlace from "./foodplace";
+import styles from './food.module.css';
 
 function isOpen(day, hour, restaurant) {
   return restaurant.OpeningHour[day] <= hour && restaurant.ClosingHour[day] >= hour
@@ -17,12 +18,12 @@ export default function ClientFoodPage({ restaurants }) {
     <div>
       <span>Only Open</span>
       <br />
-      <label id="showall" className="switch">
-        <input className="switchinput" type="checkbox" checked={openOnly} onChange={() => {setOpenOnly(!openOnly)}} />
-        <span className="switchslider"></span>
+      <label id="showall" className={styles.switch}>
+        <input className={styles.switchinput} type="checkbox" checked={openOnly} onChange={() => {setOpenOnly(!openOnly)}} />
+        <span className={styles.switchslider}></span>
       </label>
       <br />
-      <div className="foodplaces">
+      <div className={styles.foodplaces}>
         {restaurants.filter((value) => !openOnly || isOpen(day, hour, value)).map((value, index) => (
           <FoodPlace key={index} name={value.Name} openingHour={value.OpeningHour[day]} closingHour={value.ClosingHour[day]}
             rating={value.Rating} numberOfRatings={value.NumberOfRatings} description={value.description} photo={value.photoId} />
